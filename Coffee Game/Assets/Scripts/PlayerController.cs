835bf4216d8bf377d070fd39a51aaa73467ef748
Colten Berry <berrycj@hendrix.edu>
2025-02-12 16:44:47 -0600

    Circle now moves according to player input

---
+++ Coffee Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontal;
+    public float vertical;
+    public float speed = 5.0f;
+    private Rigidbody2D rigidBody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+}
